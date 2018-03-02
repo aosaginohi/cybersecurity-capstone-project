495243bd563b9f0d2cdfafba0c9f5fbc1dc8d4b3
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2018-03-02 21:47:57 +0100

    updated

--- capstone.pl
+++ capstone.pl
@@ -7,9 +7,20 @@ use strict;
 use warnings;
 use Crypt::CBC;
 use Email::Valid;
+use Email::Sender::Simple qw(sendmail);
+use Email::Sender::Transport::SMTP ();
+use Email::Simple ();
+use Email::Simple::Creator ();
+use MIME::Base64;
+use Authen::SASL;
+
+my $smtpserver = '';
+my $smtpport = 587;
+my $smtpuser   = '';
+my $smtppassword = '';
 
 my $myerror;
-my $key = 'YOU SECRET KET!';
+my $key = 'YOU SECRET KEY HERE';
 my $cipher = Crypt::CBC->new(
     -key       => $key,
     -keylength => '256',
@@ -19,8 +30,39 @@ my $cipher = Crypt::CBC->new(
 my $mysql = Mojo::mysql->new('mysql://USERNAME:PASSWORD@localhost/CAPSTONE');
 
 get '/' => 'index';
+get '/emailverification' => 'emailverification';
 get '/index.html' => 'index';
-get '/login' => 'index';
+get '/login' => 'login';
+post '/login' => sub {
+  my $c = shift;
+  
+  my $VerifyInputUsername = $c->req->body_params->param('username');
+  my $VerifyInputPassword = $c->req->body_params->param('password');
+
+  if ($VerifyInputUsername eq "" | $VerifyInputPassword eq "") 
+  {
+    my $myerror = 'Please fill in all fields.';
+    $c->stash( 
+                 myerror => $myerror, 
+                ); 
+                                 
+    return $c->render(template => 'login');
+  }
+  
+  if ($VerifyInputUsername !~ /^[a-zA-Z]+$/)
+  {
+    my $myerror = 'You can use only alphanumeric characters for "username"';
+    $c->stash( 
+                 myerror => $myerror, 
+                ); 
+                                 
+    return $c->render(template => 'login');
+  }
+  
+  my $username = $cipher->encrypt_hex($c->req->body_params->param('username'));
+  my $password = $cipher->encrypt_hex($c->req->body_params->param('password'));
+  
+};
 get '/register' => 'register';
 post '/register' => sub {
   my $c = shift;
@@ -81,7 +123,26 @@ post '/register' => sub {
   my $db = $mysql->db;
   $db->query('INSERT INTO users (LastName, FirstName, Email, UserName, Password, EmailVerification) VALUES (?, ?, ?, ?, ?, ?)', $lastname, $firstname, $email, $username, $password, $emailverification);
   
-  $c->render(template => 'index');
+my $transport = Email::Sender::Transport::SMTP->new({
+  host => $smtpserver,
+  port => $smtpport,
+  ssl => "starttls",
+  sasl_username => $smtpuser,
+  sasl_password => $smtppassword,
+});
+
+my $smtpemail = Email::Simple->create(
+  header => [
+    To      => "$VerifyInputEmail",
+    From    => 'hello@ulyaoth.net',
+    Subject => 'Cybersecurity Capston Project - Verify Email',
+  ],
+  body => "Please verify your email with this code: $emailverification\n url: http://cybersecurity-capstone-project.ulyaoth.net/emailverification",
+);
+
+sendmail($smtpemail, { transport => $transport });
+  
+  $c->redirect_to('emailverification');
   };
   
 
@@ -204,9 +265,10 @@ $('.message a').click(function(){
 </head>
 
 <body>
-<div class="login-page" action="/login" method="post">
+<div class="login-page">
+<center><font size="6"><p style="color:red"><b><%= stash "myerror" %></b><p><font></center>
   <div class="form">
-    <form class="login-form">
+    <form class="login-form" action="/login" method="post">
       <input type="text" placeholder="Username"/>
       <input type="password" placeholder="Password"/>
       <button>login</button>
@@ -217,6 +279,9 @@ $('.message a').click(function(){
 </body>
 
 </html>
+
+@@ login.html.ep
+%= include 'index'
 
 @@ register.html.ep
 <!DOCTYPE html>
@@ -346,6 +411,137 @@ $('.message a').click(function(){
 	  <input type="text" name="email" placeholder="Email"/>
       <button>register</button>
       <p class="message">Already registered? <a href="login">Sign In</a></p>
+    </form>
+  </div>
+</div>
+</body>
+
+</html>
+
+@@ emailverification.html.ep
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Cybersecurity Capstone Project - Email Verification</title>
+<style>
+@import url(https://fonts.googleapis.com/css?family=Roboto:300);
+
+.login-page {
+  width: 360px;
+  padding: 8% 0 0;
+  margin: auto;
+}
+.form {
+  position: relative;
+  z-index: 1;
+  background: #FFFFFF;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+.form input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
+}
+.form button {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #4CAF50;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.form button:hover,.form button:active,.form button:focus {
+  background: #43A047;
+}
+.form .message {
+  margin: 15px 0 0;
+  color: #b3b3b3;
+  font-size: 12px;
+}
+.form .message a {
+  color: #4CAF50;
+  text-decoration: none;
+}
+.form .register-form {
+  display: none;
+}
+.container {
+  position: relative;
+  z-index: 1;
+  max-width: 300px;
+  margin: 0 auto;
+}
+.container:before, .container:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.container .info {
+  margin: 50px auto;
+  text-align: center;
+}
+.container .info h1 {
+  margin: 0 0 15px;
+  padding: 0;
+  font-size: 36px;
+  font-weight: 300;
+  color: #1a1a1a;
+}
+.container .info span {
+  color: #4d4d4d;
+  font-size: 12px;
+}
+.container .info span a {
+  color: #000000;
+  text-decoration: none;
+}
+.container .info span .fa {
+  color: #EF3B3A;
+}
+body {
+  background: #76b852; /* fallback for old browsers */
+  background: -webkit-linear-gradient(right, #76b852, #8DC26F);
+  background: -moz-linear-gradient(right, #76b852, #8DC26F);
+  background: -o-linear-gradient(right, #76b852, #8DC26F);
+  background: linear-gradient(to left, #76b852, #8DC26F);
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;      
+}
+</style>
+<script>
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+</script>
+</head>
+
+<body>
+<div class="login-page">
+<center><font size="6"><p style="color:red"><b><%= stash "myerror" %></b><p><font></center>
+  <div class="form">
+    <form class="login-form" action="/emailverification" method="post">
+	  <input type="text" name="email" placeholder="Email"/>
+	  <input type="text" name="emailverificationcode" placeholder="Verification code"/>
+      <button>Verify your email</button>
+      <p class="message">Already verified? <a href="login">Sign In</a></p>
     </form>
   </div>
 </div>
